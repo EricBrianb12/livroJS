2a7d804f8914b658f035d1db9fc029c7dfe27f6e
Eric Brian <eric.brianb12@gmail.com>
2026-06-01 17:59:45 -0300

    exercicios-capitulo2

---
+++ capitulo02/js/exercicio-farmacia.js
@@ -0,0 +1,13 @@
+// const form = document.querySelector("form")
+// const nomeProdutoDesc = document.getElementById("nomeProduto")
+// const promocaoDesc = document.getElementById("promocao")
+
+
+// form.addEventListener("submit", (e) => {
+//   const nomeProduto = form.inProduto.value
+//   const preco = Number(form.inPreco.value)
+
+//   console.log(nomeProduto)
+
+//   e.preventDefault()
+// })
