a5c29ecb049adb66206d7613ab5647d0bc1016cd
Eric Brian <eric.brianb12@gmail.com>
2026-06-10 21:14:49 -0300

    adicionado as respostas direto no projeto e feito o exercicio 3 do capitulo 4

--- capitulo04/exercicios/ex03/ex03.js
+++ capitulo04/exercicios/ex03/ex03.js
@@ -1,28 +1,44 @@
-const form = document.querySelector("form")
-const tempo = document.querySelector("#tempo")
-const troco = document.querySelector("#troco")
+const form = document.querySelector("form");
+const resp1 = document.querySelector("#outTempo");
+const resp2 = document.querySelector("#outTroco");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const valor = Number(form.inValor.value)
+  const valor = Number(form.inValor.value);
+  let sobra;
 
-  if (valor < 1 || valor > 3) {
-    alert("Valor mínimo deve ser de R$: 1.00 e o valor máximo de R$: 3.00")
-    form.inValor.focus()
-    return
+  //caso o saldo seja insuficiente
+  if (valor < 1.00) {
+    alert("Valor mínimo deve ser de R$: 1.00");
+    form.inValor.focus();
+    return;
   }
 
-  if (valor < 1.75) {
-    tempo.innerText = `Tempo: 30 minutos`;
+  //declara as variaveis
+  let tempo;
+  let troco;
+
+  //condições para verificar, tempo e troco
+  if (valor >= 3.00) {
+    tempo = 120;
+    troco = valor - 3.00;
+  } else if (valor >= 1.75) {
+    tempo = 60
+    troco = valor - 1.75
   } else {
-    if (valor <= 3) {
-      tempo.innerText = `Tempo: 30 minutos`;
-    }
+    tempo = 30
+    troco = valor - 1.00
   }
 
+  //limpa o texto da tela
+  resp2.innerText = ""
+
+  //exibe as respostas
+  resp1.innerText = `Tempo: ${tempo} minuto`
+  if (troco > 0) {
+    resp2.innerText = `Troco R$: ${troco.toFixed(2)}`
+  }
+});
 
 
-
-
-})
