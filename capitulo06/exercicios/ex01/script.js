7ec53d862dda009790546cf48dd401e76b547908
Eric Brian <eric.brianb12@gmail.com>
2026-07-09 18:00:25 -0300

    finalizando o ex01 do capitulo 06

--- capitulo06/exercicios/ex01/script.js
+++ capitulo06/exercicios/ex01/script.js
@@ -1,5 +1,49 @@
-/* 
-Elaborar um programa para gerar uma tabela com os jogos de uma fase eliminatória de um campeonato. O programa deve conter três funções (a serem executadas no evento click de cada botão) para: 1) validar o preenchimento, adicionar um clube ao vetor e listar os clubes; 2) listar os clubes (se houver); 3) montar a tabela de jogos, no formato primeiro x último, segundo x penúltimo e assim por diante. Exibir mensagem e não listar a tabela de jogos, caso o número de clubes informados seja ímpar. A Figura 6.14 ilustra a página gerada com a tabela de jogos.
-*/
-
 const form = document.querySelector("form")
+const resp = document.querySelector("pre")
+const clubes = []
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const clube = form.inClube.value
+  clubes.push(clube)
+
+
+  form.btnListar.dispatchEvent(new Event("click"))
+
+  form.inClube.value = ""
+  form.inClube.focus()
+})
+
+form.btnListar.addEventListener("click", () => {
+  let mensagem = `Times cadastrados: \n` + "-".repeat(40) + "\n"
+  if (clubes.length === 0) {
+    alert("Sem times cadastrados para ser listado")
+    inClube.focus()
+    return
+  } else {
+    const clube = clubes.join("\n")
+    resp.innerText = `${mensagem}${clube}`
+  }
+})
+
+form.btnTabela.addEventListener("click", () => {
+  const tam = clubes.length
+
+  if (tam === 0 || tam % 2 === 1) {
+    alert("Deve haver número par de clubes")
+    inClube.focus()
+    return
+  }
+
+  let jogos = ""
+
+  const ultimo = tam - 1
+
+  for (let i = 0; i < tam / 2; i++) {
+    jogos += clubes[i] + " x " + clubes[ultimo - i] + "\n"
+  }
+
+  resp.innerText = jogos
+
+})
